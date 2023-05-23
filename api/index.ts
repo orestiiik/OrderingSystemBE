37c7fa707b238ee768bec1cbdc9c9ef1dbc45aa0
orestgluchy <orest.gluchy@webgang.io>
2023-05-23 22:30:53 +0200

    Tried to fix

--- api/index.ts
+++ api/index.ts
@@ -1,48 +1,91 @@
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import typeDefs from "./graphql/schema/typeDefs";
-import resolvers from "./graphql/schema/resolvers";
-import https from 'https';
-import fs from 'fs';
-import http from "http";
+// import express from "express";
+// import { ApolloServer } from "apollo-server-express";
+// import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import td from "./graphql/schema/typeDefs";
+import rs from "./graphql/schema/resolvers";
+// import https from 'https';
+// import fs from 'fs';
+// import http from "http";
+//
+// require('dotenv').config();
+//
+// const app = express();
+// app.use(express.json());
+//
+// // const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req })});
+//
+// const httpServer = http.createServer(app);
+//
+// // const tlsOptions = {
+// //     key: fs.readFileSync('./src/tls/localhost-key.pem'),
+// //     cert: fs.readFileSync('./src/tls/localhost.pem')
+// // };
+// // async function startServer() {
+// //     await server.start();
+// //     server.applyMiddleware({ app });
+// //
+// //     https.createServer(tlsOptions, app).listen(process.env.PORT, () => {
+// //         console.log(`🚀 Server ready at https://localhost:${process.env.PORT}${server.graphqlPath}`);
+// //     });
+// // }
+// //
+// // startServer();
+//
+// const startApolloServer = async(app, httpServer) => {
+//     const server = new ApolloServer({
+//         context: ({ req }) => ({ req }),
+//         typeDefs,
+//         resolvers,
+//         plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+//     });
+//
+//     await server.start();
+//     console.log(`🚀 Server ready at https://localhost:${process.env.PORT}${server.graphqlPath}`)
+//     server.applyMiddleware({ app });
+// }
+//
+// startApolloServer(app, httpServer);
+// export default httpServer;
 
-require('dotenv').config();
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+import express from "express";
+import cors from "cors";
 
 const app = express();
-app.use(express.json());
 
-// const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req })});
+app.use(cors());
+app.use(express.json());
+app.listen(4040)
 
 const httpServer = http.createServer(app);
 
-// const tlsOptions = {
-//     key: fs.readFileSync('./src/tls/localhost-key.pem'),
-//     cert: fs.readFileSync('./src/tls/localhost.pem')
-// };
-// async function startServer() {
-//     await server.start();
-//     server.applyMiddleware({ app });
-//
-//     https.createServer(tlsOptions, app).listen(process.env.PORT, () => {
-//         console.log(`🚀 Server ready at https://localhost:${process.env.PORT}${server.graphqlPath}`);
-//     });
-// }
-//
-// startServer();
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
 
+const resolvers = {
+    Query: {
+        hello: () => "world",
+    },
+};
+
+console.log(td)
+console.log(rs)
 const startApolloServer = async(app, httpServer) => {
     const server = new ApolloServer({
-        context: ({ req }) => ({ req }),
         typeDefs,
-        resolvers,
+        resolvers: rs,
         plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     });
 
     await server.start();
-    console.log(`🚀 Server ready at https://localhost:${process.env.PORT}${server.graphqlPath}`)
     server.applyMiddleware({ app });
 }
 
 startApolloServer(app, httpServer);
+
 export default httpServer;
